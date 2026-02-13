65ff65ee710712cac7b510aee1b67d1124faa354
ryzenexe1 <140967673+ryzenexe1@users.noreply.github.com>
2026-02-13 11:21:08 +0500

    Update task01.c

--- Lab04/task01.c
+++ Lab04/task01.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main()
+{
+	float att;
+	printf("Enter our practice attendance percentage: \n");
+	scanf("%f", &att);
+	if (att>=75)
+	{
+		printf("Selected for tournament");
+	}
+	else 
+	{
+		printf("Not selected");
+	}
+}
